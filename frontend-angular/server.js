c74a1f0b73e5fb70be3ab453e6eacf1572cd1d2d
SILGA Pinguedwindé fabrice <sliga.brice@gmail.com>
2021-09-10 15:27:29 +0200

    MBTI-54 : fix the prod backend url

--- frontend-angular/server.js
+++ frontend-angular/server.js
@@ -4,13 +4,19 @@ const path = require("path");
 
 const app = express();
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 8080;
 
-app.use(express.static("./public"));
+app.use(express.static(__dirname + "/public"));
 
 app.get("/*", (req, res) => res.sendFile(path.join(__dirname)));
 
 const server = http.createServer(app);
 
-server.listen(port, () => console.log("MBTI Assessment App is running..."));
-console.log(__dirname);
+server.listen(port, () => {
+  console.log({
+    PORT: port,
+    filename: __filename,
+    public: __dirname+'/public'
+  });
+  console.log("MBTI Assessment App is running...");
+});
--- frontend-angular/src/environments/environment.prod.ts
+++ frontend-angular/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 
 export const environment = {
    production: true,
-   API_URL: "undefined",
+   API_URL: "https://app-5b8fb044-964e-4423-8f49-eff3f22f8bed.cleverapps.io",
 };
